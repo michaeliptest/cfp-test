01c83b3d7fb6f1488711933146631822dd334e71
Michael Ip <michaelip@Michaels-MacBook-Air.local>
2020-05-19 19:13:06 -0400

    updated top details

--- src/components/TopDetails.jsx
+++ src/components/TopDetails.jsx
@@ -1,38 +1,44 @@
 import React from 'react'
 
-function TopDetails(){
+function TopDetails() {
 
     const style = {
         details: {
             display: "inline-block",
-            margin: '5px'
+            margin: '5px 15px 5px 0px'
         },
         pstyle: {
             margin: "0"
+        },
+        boldtext: {
+            fontWeight: "bold"
         }
     }
 
-    return(
+    return (
         <>
-        <p style={style.pstyle}>Title</p>
-        <p style={style.pstyle}>Name</p>
+            <p style={{ ...style.pstyle, ...style.boldtext}}>Title</p>
+            <p style={style.pstyle}>Name</p>
 
-        <div style={style.details}>
-        <p style={style.pstyle}>Email</p>
-        <p style={style.pstyle}>Email Address</p>
-        </div>
-        <div style={style.details}>
-        <p style={style.pstyle}>Phone</p>
-        <p style={style.pstyle}>123-456-7890</p>
-        </div>
-        <div style={style.details}>
-        <p style={style.pstyle}>Last Login</p>
-        <p style={style.pstyle}>Never</p>
-        </div>
-        <div style={style.details}>
-        <p style={style.pstyle}>Login Count</p>
-        <p style={style.pstyle}>0</p>
-        </div>
+            <div style={style.details}>
+                <p style={{ ...style.pstyle, ...style.boldtext}}>Email</p>
+                <p style={style.pstyle}>Email Address</p>
+            </div>
+
+            <div style={style.details}>
+                <p style={{ ...style.pstyle, ...style.boldtext}}>Phone</p>
+                <p style={style.pstyle}>123-456-7890</p>
+            </div>
+
+            <div style={style.details}>
+                <p style={{ ...style.pstyle, ...style.boldtext}}>Last Login</p>
+                <p style={style.pstyle}>Never</p>
+            </div>
+
+            <div style={style.details}>
+                <p style={{ ...style.pstyle, ...style.boldtext}}>Login Count</p>
+                <p style={style.pstyle}>0</p>
+            </div>
         </>
     )
 }
